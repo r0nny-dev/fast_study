6187368adb5b5b3b6e8866b1fe65b15f14005f6e
r0nny-dev <rony.nascimento18@gmail.com>
2024-07-14 14:39:37 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,76 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_study.app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    # Arrange
-    client = TestClient(app)
-
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     # Act
     response = client.get('/')
 
     # Assert
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World - Root'}
+
+
+def test_create_user(client):
+    # Act
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    # Assert
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    # Act
+    response = client.get('/users/')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    # Act
+    response = client.put(
+        'users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    # Act
+    response = client.delete('/users/1')
+
+    # Assert
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
